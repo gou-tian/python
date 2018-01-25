cf728e0f23b66f9def7566611cfe0cb81b875743
gousky <gousky@126.com>
2018-01-25 18:04:51 +0800

    初始化博客页面 <一边学习Flask，一边开发博客>

---
+++ blog/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, make_response, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    response = make_response('这是页面缓存！')
+    response.set_cookie('dumpling', 'blog')
+    return render_template('index.html', browser=user_agent, cookies=response)
+
+
+@app.route('/about')
+def about():
+    return '<h1>饺子的介绍</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
+
+if __name__ == '__main__':
+    # host='0.0.0.0',
+    # manager.run()
+    bt = Bootstrap(app)
+    app.run(debug=True)
